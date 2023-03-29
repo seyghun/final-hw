d73bebd4211489e10c79d49557004b860d2e2ee8
seyghun <yaps.gbk@gmail.com>
2023-03-29 21:11:28 +0300

    fixed HW-7

--- HW-7/js/script.js
+++ HW-7/js/script.js
@@ -2,7 +2,6 @@ const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
-
 function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
